fe79978cc6e8b113ec659517b5fd6a76da97406a
SemionStepan <95352195+SemionStepan@users.noreply.github.com>
2023-07-03 12:44:30 +0300

    Added some things in header

--- figure/figure.h
+++ figure/figure.h
@@ -13,35 +13,42 @@ public:
 
 class Rectangle : public Figure
 {
-    // У прямоугольника всегда 4 угла, все они по 90 градусов
+    // У прямоугольника всегда 4 угла, все они по 90 градусов, есть 4 стороны, которые попарно равны
     int width;
     int height;
 public:
-    Rectangle(int width, int height) {}
+    Rectangle(int width, int height) { }
     inline double Square() { return width * height; }
-    inline double Perimeter() { return (2 * width) + (2 * height);}
+    inline double Perimeter() { return (2 * width) + (2 * height); }
+    inline int getWidth() { return width; }
+    inline int getHeight() { return height; }
 };
 
 class Circle : public Figure
 {
-    // У окружности 0 углов, есть константа - число пи
+    // У окружности 0 углов, есть 1 сторона и константа - число пи
     double radius;
     const double pi = 3.14;
 public:
-    Circle(double radius) {}
-    inline double Square() {return radius * radius * pi; }
-    inline double Perimeter() {return 2 * radius * pi;}
+    Circle(double radius) { }
+    inline double Square() { return radius * radius * pi; }
+    inline double Perimeter() { return 2 * radius * pi; }
+    inline double getRadius() { return radius; }
 };
 
 class Triangle : public Figure
 {
+    // У треугольника 3 угла, они могут быть от 1 до 179 градусов, 3 стороны
     int a;
     int b;
     int c;
 public:
-    Triangle(int a, int b, int c) {}
-    inline double Square(){ double p = (a + b + c) / 2; return sqrt(p*(p - a)*(p - b)*(p - c)); };
-    inline double Perimeter() { return a+b+c; }
+    Triangle(int a, int b, int c) { }
+    inline double Square(){ double p = ( a + b + c ) / 2; return sqrt(p*(p - a)*(p - b)*(p - c)); };
+    inline double Perimeter() { return a + b + c; }
+    inline int getA() { return a; }
+    inline int getB() { return b; }
+    inline int getC() { return c; }
 };
 /*
 class Printer
